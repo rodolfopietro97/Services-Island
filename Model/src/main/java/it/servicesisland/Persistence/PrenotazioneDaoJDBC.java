3ed54d3464c82e148ec0b51eb01c8f5febd20059
Tiziana <Tiziana@160.97.141.154>
2019-01-19 20:40:53 +0100

    added dao prenotazione

---
+++ Model/src/main/java/it/servicesisland/Persistence/PrenotazioneDaoJDBC.java
@@ -0,0 +1,132 @@
+package it.servicesisland.Persistence;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import it.servicesisland.Model.*;
+
+
+/**
+ * 
+ * @author Tiziana
+ *
+ */
+public class PrenotazioneDaoJDBC {
+
+	/**
+	 * Instance of DataSource
+	 */
+	private DataSource dataSource;
+
+	/**
+	 * Constructor with parameters
+	 * @param dataSource of connection
+	 */
+	public PrenotazioneDaoJDBC(DataSource dataSource) {
+		this.dataSource = dataSource;
+		//dataSource=new DataSource("jdbc:postgresql://localhost:5432/services_island","postgres","postgres");
+	}
+	
+	/**
+	 * Add new booking in the database
+	 * @param prenotazione to save
+	 */
+	public void save(Prenotazione prenotazione) {
+		
+		Connection connection = this.dataSource.getConnection();
+		try {
+			
+			 
+			String insert = "insert into prenotazione(orario, giorno, utente, servizio) values (?,?,?,?)";
+			PreparedStatement statement = connection.prepareStatement(insert);
+			
+			statement.setTime(1, prenotazione.getOrario());
+			statement.setDate(2, prenotazione.getGiorno());
+			statement.setInt(3, prenotazione.getUtente());
+			statement.setInt(4, prenotazione.getServizio());
+						
+			statement.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			if (connection != null) {
+				try {
+					connection.rollback();
+				} catch(SQLException excep) {
+					excep.printStackTrace();
+				}
+			} 
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * finds a prenotazione by primary key
+	 * @param idUs
+	 * @param idSe
+	 * @return the found instance of prenotazione
+	 */
+	public Prenotazione findByPrimaryKey(Long idUs, Long idSe) {
+		
+		Connection connection = this.dataSource.getConnection();
+		Prenotazione prenotazione = null;
+		try {
+			PreparedStatement statement;
+			String query = "select * from prenotazione where utente = ? and servizio = ?";
+			statement = connection.prepareStatement(query);
+			statement.setLong(1, idUs);
+			statement.setLong(2, idSe);
+			ResultSet result = statement.executeQuery();
+			if (result.next()) {
+				prenotazione = new Prenotazione();
+				prenotazione.setGiorno(result.getDate("giorno"));
+				prenotazione.setOrario(result.getTime("orario"));
+				prenotazione.setUtente(result.getInt("utente"));
+				prenotazione.setServizio(result.getInt("servizio"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}	
+		return prenotazione;
+	}
+	
+	/**
+	 * Delete a given instance of Prenotazione, finding it by primary key	
+	 * @param id of user, id of service
+	 */
+	public void delete(Long idUs, Long idSe) {
+			
+			Connection connection = this.dataSource.getConnection();
+			try {
+			PreparedStatement statement;
+			String query = "delete from prenotazione where utente = ? and servizio = ?";
+			statement = connection.prepareStatement(query);
+			statement.setLong(1, idUs);
+			statement.setLong(2, idSe);
+			statement.execute();
+			
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+}
